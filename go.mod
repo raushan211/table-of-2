4933c349e4bc0b2f5f8b117bc8dd734b1c203105
RAUSHAN KUMAR <raushankumar@Prince-Raj.local>
2022-06-08 14:41:12 +0530

    table of 2

---
+++ go.mod
@@ -0,0 +1,3 @@
+module table2
+
+go 1.18
---
+++ table2.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("2*1=2")
+	fmt.Println("2*2=4")
+	fmt.Println("2*3=6")
+	fmt.Println("2*4=8")
+	fmt.Println("2*5=10")
+	fmt.Println("2*6=12")
+	fmt.Println("2*7=14")
+	fmt.Println("2*8=16")
+	fmt.Println("2*9=18")
+	fmt.Println("2*10=20")
+}
